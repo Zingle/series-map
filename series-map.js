448bd62bd9c70556efea000c80c7e22bf1dd1f87
Richard Remer <rich.remer@zingle.me>
2017-12-27 17:32:04 -0800

    refactor for classical inheritance

--- series-map.js
+++ series-map.js
@@ -1,109 +1,110 @@
+const roots = new WeakMap();
+
 /**
  * Map a series of values to another distinct value.
  * @returns {SeriesMap}
  */
 function SeriesMap() {
-    const lengthMap = new Map();
-
     if (this.constructor !== SeriesMap) {
         throw new TypeError("Constructor SeriesMap requires 'new'");
     }
 
-    return {
-        /**
-         * Return true if map contains a series.
-         * @param {array} series
-         * @returns {boolean}
-         */
-        has(series) {
-            if (!(series instanceof Array)) return false;
+    roots.set(this, new Map());
+    return this;
+}
 
-            var map = lengthMap,
-                keys = [series.length].concat(series),
-                i;
+/**
+ * Return true if map contains a series.
+ * @param {array} series
+ * @returns {boolean}
+ */
+SeriesMap.prototype.has = function(series) {
+    if (!(series instanceof Array)) return false;
 
-            for (i = 0; i < keys.length; i++) {
-                if (!map.has(keys[i])) return false;
-                map = map.get(keys[i]);
-            }
+    var map = roots.get(this),
+        keys = [series.length].concat(series),
+        i;
 
-            return true;
-        },
-
-        /**
-         * Return the value mapped to a series.
-         * @param {array} series
-         * @returns {*}
-         */
-        get(series) {
-            if (!(series instanceof Array)) return undefined;
-
-            var map = lengthMap,
-                keys = [series.length].concat(series),
-                i;
-
-            for (i = 0; i < keys.length; i++) {
-                if (!map.has(keys[i])) return undefined;
-                map = map.get(keys[i]);
-            }
-
-            return map;
-        },
-
-        /**
-         * Map a series to a value.
-         * @param {array} series
-         * @param {*} value
-         */
-        set(series, value) {
-            if (!(series instanceof Array)) {
-                throw new TypeError("series must be an Array");
-            }
-
-            var map = lengthMap,
-                keys = [series.length].concat(series),
-                i;
-
-            for (i = 0; i < keys.length - 1; i++) {
-                if (!map.has(keys[i])) map.set(keys[i], new Map());
-                map = map.get(keys[i]);
-            }
-
-            map.set(keys[keys.length-1], value);
-        },
-
-        /**
-         * Delete series mapped value.
-         * @param {array} series
-         */
-        delete(series) {
-            if (!(series instanceof Array)) return;
-
-            var keys = [series.length].concat(series),
-                map = lengthMap,
-                maps = [],
-                i
-
-            for (i = 0; i < keys.length; i++) {
-                if (!map.has(keys[i])) return;
-                maps.push(map);
-                map = map.get(keys[i]);
-            }
-
-            for (i = keys.length - 1; i >= 0; i--) {
-                map = maps[i];
-                map.delete(keys[i]);
-                if (map.size) return;
-            }
-        },
-
-        /**
-         * Clear mapped values.
-         */
-        clear() {
-            lengthMap.clear();
-        }
+    for (i = 0; i < keys.length; i++) {
+        if (!map.has(keys[i])) return false;
+        map = map.get(keys[i]);
     }
+
+    return true;
+},
+
+/**
+ * Return the value mapped to a series.
+ * @param {array} series
+ * @returns {*}
+ */
+SeriesMap.prototype.get = function(series) {
+    if (!(series instanceof Array)) return undefined;
+
+    var map = roots.get(this),
+        keys = [series.length].concat(series),
+        i;
+
+    for (i = 0; i < keys.length; i++) {
+        if (!map.has(keys[i])) return undefined;
+        map = map.get(keys[i]);
+    }
+
+    return map;
+},
+
+/**
+ * Map a series to a value.
+ * @param {array} series
+ * @param {*} value
+ */
+SeriesMap.prototype.set = function(series, value) {
+    if (!(series instanceof Array)) {
+        throw new TypeError("series must be an Array");
+    }
+
+    var map = roots.get(this),
+        keys = [series.length].concat(series),
+        i;
+
+    for (i = 0; i < keys.length - 1; i++) {
+        if (!map.has(keys[i])) map.set(keys[i], new Map());
+        map = map.get(keys[i]);
+    }
+
+    map.set(keys[keys.length-1], value);
+},
+
+/**
+ * Delete series mapped value.
+ * @param {array} series
+ */
+SeriesMap.prototype.delete = function(series) {
+    if (!(series instanceof Array)) return;
+
+    var keys = [series.length].concat(series),
+        map = roots.get(this),
+        maps = [],
+        i
+
+    for (i = 0; i < keys.length; i++) {
+        if (!map.has(keys[i])) return;
+        maps.push(map);
+        map = map.get(keys[i]);
+    }
+
+    for (i = keys.length - 1; i >= 0; i--) {
+        map = maps[i];
+        map.delete(keys[i]);
+        if (map.size) return;
+    }
+},
+
+/**
+ * Clear mapped values.
+ */
+SeriesMap.prototype.clear = function() {
+    roots.get(this).clear();
 }
 
 module.exports = SeriesMap;
